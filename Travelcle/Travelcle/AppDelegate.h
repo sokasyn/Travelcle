840e3e544e2280e7e6dca0e3e0cba4adfbf4ac9f
sokasyn <sokasyn@qq.com>
2016-03-18 13:10:21 +0800

    Project created

---
+++ Travelcle/Travelcle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Travelcle
+//
+//  Created by Samson Tseng on 16/3/18.
+//  Copyright © 2016年 Samson Tseng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Travelcle/Travelcle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Travelcle
+//
+//  Created by Samson Tseng on 16/3/18.
+//  Copyright © 2016年 Samson Tseng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
