302c396dff42d5d7d99073efa3ce35b2255dc7ed
sokasyn <sokasyn@qq.com>
2016-03-18 14:16:07 +0800

    增加登录模块层次

---
+++ Travelcle/Travelcle/Login/Controller/LoginController.h
@@ -0,0 +1,16 @@
+//
+//  LoginController.h
+//  Travelcle
+//
+//  Created by Samson Tseng on 16/3/18.
+//  Copyright © 2016年 Samson Tseng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TCUser.h"
+
+@interface LoginController : UIViewController
+
+@property (retain, nonatomic) TCUser *user;
+
+@end
---
+++ Travelcle/Travelcle/Login/Controller/LoginController.m
@@ -0,0 +1,88 @@
+//
+//  LoginController.m
+//  Travelcle
+//
+//  Created by Samson Tseng on 16/3/18.
+//  Copyright © 2016年 Samson Tseng. All rights reserved.
+//
+
+#import "LoginController.h"
+
+@interface LoginController ()
+
+@end
+
+@implementation LoginController
+
+@synthesize user = user_;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self initData];
+    [self initCompenents];
+    [self btnLoginClicked:nil];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark -User Interface
+- (void)initData{
+    self.user.name = @"Samson";
+    self.user.password = @"1234";
+}
+
+- (void)initCompenents{
+    
+}
+
+#pragma mark -IBActions
+- (void)btnLoginClicked:(id)sender{
+    NSString *userName = @"sam";
+    NSString *password = @"123";
+    [self loginWithUserName:userName password:password];
+}
+
+#pragma mark -UserActions
+#pragma mark -login
+- (void)login:(TCUser *)user{
+    [self loginWithUserName:user.name password:user.password];
+}
+
+- (void)loginWithUserName:(NSString *)userName password:(NSString *)password{
+    if ([userName isEqualToString:self.user.name] && [password isEqualToString:self.user.password]) {
+        NSLog(@"OK!");
+    }else{
+        NSLog(@"login denied!");
+    }
+}
+
+#pragma mark -Register
+- (void)userRegister{
+    
+}
+
+#pragma mark -Forget password
+- (void)forgetPassword{
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark -Storyboard
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    
+}
+
+@end
